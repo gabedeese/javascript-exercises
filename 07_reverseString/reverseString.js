1df7c8f41b606c9d41690e6ba428732843b9de9c
gabedeese <182055125+gabedeese@users.noreply.github.com>
2025-12-14 13:07:26 -0500

    Add solution to reverseString.js

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let result = '';
+  while (str.length > 0) {
+    result += str.slice(-1);
+    str = str.slice(0, -1);
+  }
+  return result;
 };
 
 // Do not edit below this line
