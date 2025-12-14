317149336aa7dff28fc2879dcac1c31783ac0e97
gabedeese <182055125+gabedeese@users.noreply.github.com>
2025-12-14 12:22:38 -0500

    Add solution to the .js file

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(str, rep) {
+  if (rep < 0){
+    return "ERROR";
+  }
+  else if (rep === 0){
+    return '';
+  }
+  else {
+    let i = 1;
+    let result = str;
+    while (i < rep){
+      result += str;
+      i++;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
