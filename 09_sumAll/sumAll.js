3737812e73b041b3e3b97c381a3ef062b3ace5eb
gabedeese <182055125+gabedeese@users.noreply.github.com>
2025-12-16 02:42:37 -0500

    Add solution to sumAll.js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+  if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) {
+    return "ERROR";
+  }
 
+  else{
+    if (x > y) {
+      let z = x;
+      x = y;
+      y = z;
+    }
+
+    let sum = 0;
+
+    for (let i = x; i <= y; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
