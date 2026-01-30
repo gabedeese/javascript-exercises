489105e591aa1a55b2480857a2e1b037d5c76c62
gabedeese <182055125+gabedeese@users.noreply.github.com>
2026-01-30 13:43:41 -0500

    Add solution to problem 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let rawAlphaNum = str
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "");
 
+  let rev = rawAlphaNum
+    .split("")
+    .reverse()
+    .join("");
+
+  return (rawAlphaNum === rev);
 };
 
 // Do not edit below this line
