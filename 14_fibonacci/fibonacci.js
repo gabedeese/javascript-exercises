684235adf59654797c63aed375578dbd5da1f70f
gabedeese <182055125+gabedeese@users.noreply.github.com>
2026-01-30 14:41:01 -0500

    Add solution to problem 14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  n = parseInt(n);
 
+  if (n <= 0) {
+    if (n == 0) return 0;
+    else return "OOPS";
+  }
+
+  let a = 0;
+  let b = 1;
+  for (let i = 1; i < n; i++) {
+    let curr = a + b;
+    a = b;
+    b = curr;
+  }
+  return b;
 };
 
 // Do not edit below this line
