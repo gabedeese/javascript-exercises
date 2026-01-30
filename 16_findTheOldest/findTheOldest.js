db4f3320c7fb4beda102a7774bc4aae70ad1bfc5
gabedeese <182055125+gabedeese@users.noreply.github.com>
2026-01-30 15:28:37 -0500

    Add solution to the problem 16 findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
+const findTheOldest = function(arr) {
+  const oldestPerson = arr.sort((a,b) => {
+    return getAge(b.yearOfBirth, b.yearOfDeath) - getAge(a.yearOfBirth, a.yearOfDeath);
+  })
+  return oldestPerson[0];
 };
 
 // Do not edit below this line
